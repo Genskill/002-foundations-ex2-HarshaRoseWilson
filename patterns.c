4879a3ffed5c4b32653eaa8a8eeb0ed9c5c9077b
HarshaRoseWilson <84075039+HarshaRoseWilson@users.noreply.github.com>
2021-05-20 19:12:15 +0530

    patterns.c
    Exercise 1

---
+++ patterns.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+
+int pattern;
+int height;
+
+pattern = get_int("Enter type of pattern (1 or 2) ");
+height = get_int("Number of rows ");
+
+if (pattern==1) {
+ 
+ for (int row=1;row<=height;row++) {
+      
+     for(int column=row;column<=height;column++) {
+       printf("#");
+       }
+ printf("\n");
+}
+
+}else { 
+
+ for (int row=1;row<=height;row++) {
+   
+    for(int i=row;i<height;i++){
+    printf(" ");
+    }
+for (int column=1;column<=row;column++){
+   printf("#");
+}
+printf("\n");
+}
+ }
+}
